f143ae88bc55fbf7fda724acc8925a9dc66ac925
DenisGizatulin <denis_gizatulin@outlook.com>
2024-11-25 11:46:34 +0300

    added default settings for the game

--- snake_game.py
+++ snake_game.py
@@ -1,5 +1,6 @@
 import pygame
-
+import time
+import random
 
 def Snake_game():
     snake_speed = 15
@@ -25,3 +26,24 @@ def Snake_game():
     # FPS (frames per second) контроллер
     fps = pygame.time.Clock()
 
+    # Определяем позицию по умолчанию для змейки
+    snake_position = [100, 50]
+
+    # Определяем первые 4 блока змейки
+    snake_body = [[100, 50],
+                  [90, 50],
+                  [80, 50],
+                  [70, 50]
+                  ]
+    # Позиция яблока
+    fruit_position = [random.randrange(1, (window_x // 10)) * 10,
+                      random.randrange(1, (window_y // 10)) * 10]
+
+    fruit_spawn = True
+
+    # Змейка по уцмолчанию двигается вправо
+    direction = 'RIGHT'
+    change_to = direction
+
+    # Количество набранных очков
+    score = 0
