94c808ef485f177ecd26dceb0aee72b4819bae4d
DenisGizatulin <denis_gizatulin@outlook.com>
2024-11-25 14:13:18 +0300

    game_over function added

--- snake_game.py
+++ snake_game.py
@@ -63,3 +63,42 @@ def Snake_game():
         # отображаем текст
         game_window.blit(score_surface, score_rect)
 
+    # Функция конца игры
+    def game_over():
+
+        try:
+            with open('highscore.txt', 'r') as f:
+                current_highscore = int(f.read())
+        except (FileNotFoundError, ValueError):
+            current_highscore = 0  # Значение счёта по умолчанию
+
+            # Сравнение нынешнего результата с максимальным
+        if score > current_highscore:
+            # Если нынешний результат больше, чем максимальный, то записываем нынешний результат в файл
+            with open('highscore.txt', 'w') as f:
+                f.write('%d' % score)
+
+        my_font = pygame.font.SysFont('times new roman', 50)
+
+        # Создаём текстовую поверхность, на которой будем отображать результат
+        game_over_surface = my_font.render(
+            'Вы набрали : ' + str(score) + ' очков', True, red)
+
+        # Создаём четырёхугольник
+        game_over_rect = game_over_surface.get_rect()
+
+        # Указываем позицию текста
+        game_over_rect.midtop = (window_x / 2, window_y / 4)
+
+        # Это будет выводить текст на экран
+        game_window.blit(game_over_surface, game_over_rect)
+        pygame.display.flip()
+
+        # Устанавливаем задержку перед выходом
+        time.sleep(2)
+
+        # Выключаем Pygame
+        pygame.quit()
+
+        # Выход из программы
+        quit()
